83f58304fdb35e402891dc5845fd88e89d4e8275
JCluzet <55356071+JCluzet@users.noreply.github.com>
2021-02-11 14:28:07 +0100

    Norminette fix

--- bonus/display/draw.c
+++ bonus/display/draw.c
@@ -6,7 +6,7 @@
 /*   By: jcluzet <jo@cluzet.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 19:25:03 by jcluzet           #+#    #+#             */
-/*   Updated: 2021/02/11 14:24:07 by jcluzet          ###   ########.fr       */
+/*   Updated: 2021/02/11 14:27:03 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,14 @@ void	drawloop(t_display *display, int y, int x)
 		display->texywall = (int)display->texpos &
 		(display->heighttext[display->textnum] - 1);
 		display->texpos += display->stepdraw;
-		if(display->texywall < 64 && display->texxwall < 64)
+		if (display->texywall < 64 && display->texxwall < 64)
 		{
 			display->color = *(unsigned int *)(display->ptr[display->textnum] +
 			display->s_line2[display->textnum] * display->texywall +
 			display->texxwall * (display->bpp2[display->textnum] / 8));
-		display->color = addsmog3(display->perwalldist,
-		display, display->color);
-		put_pxl(display, x, y, display->color);
+			display->color = addsmog3(display->perwalldist,
+			display, display->color);
+			put_pxl(display, x, y, display->color);
 		}
 		y++;
 	}
